e90cdf2deb83fd057ca49131e73aeefc2c24c666
Michael Tang <mtang8264@gmail.com>
2019-04-04 19:35:48 -0400

    Ha

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public Vector2Int pos;
+    public enum Type { RED, ORANGE, BLUE, YELLOW, GREEN, PURPLE, POINT };
+    public Type type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 goalPos = new Vector3(pos.x + Grid.me.offset.x, pos.y + Grid.me.offset.y);
+        transform.position = Vector3.Lerp(transform.position, goalPos, 0.1f);
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public static Grid me;
+    public List<Cell> cells;
+    public Vector2Int dimensions;
+    public Vector2 offset;
+    public GameObject cellPrefab;
+
+    private void Awake()
+    {
+        me = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InitGrid()
+    {
+        for (int x = 0; x < dimensions.x; x++)
+        {
+            for (int y = 0; y < dimensions.y; y++)
+            {
+                GameObject obj = Instantiate(cellPrefab);
+                cells.Add(obj.GetComponent<Cell>());
+                obj.GetComponent<Cell>().pos = new Vector2Int(x, y);
+            }
+        }
+    }
+}
